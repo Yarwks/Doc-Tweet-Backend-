a3cb9a6783c2c2572059c8754dc49fdd7df6ad6d
Ray Wekesa <ray.wekesa@student.moringaschool.com>
2026-07-30 16:00:26 +0300

    Added Jwt tokens and updated models

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,23 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-from flask_migrate import Migrate
-from flask_login import LoginManager
 from flask_jwt_extended import JWTManager
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 
-app.config['JWT_SECRET_KEY'] = 'my-jwt-secret-key'
-app.config['SECRET_KEY'] = 'my-secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
+app.config["JWT_SECRET_KEY"] = "my-jwt-secret-key"
+app.config["SECRET_KEY"] = "my-secret-key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["UPLOAD_FOLDER"] = "uploads"
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 jwt = JWTManager(app)
-login_manager.login_view = 'login'
+
 
 from app import routes
--- app/models.py
+++ app/models.py
@@ -1,32 +1,21 @@
 from flask_login import UserMixin
+
 from app import db
 from datetime import datetime
 from sqlalchemy import func
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
-    avatar_url = db.Column(db.String(255), nullable=True)
-    created_at = db.Column(db.DateTime, default=func.now())
+    role = db.Column(db.String(80), nullable=False)
+    is_verified = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f"<User {self.username}>"
 
-class Doctor(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    institution = db.Column(db.String(120), nullable=False)
-    password_hash = db.Column(db.String(255), nullable=False)
-    avatar_url = db.Column(db.String(255), nullable=True)
-    is_verified = db.Column(db.Boolean, default=False)
-    specialization = db.Column(db.String(120), nullable=True)
-    created_at = db.Column(db.DateTime, default=func.now())
-
-    def __repr__(self) -> str:
-        return f"<Doctor {self.username}"
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -42,6 +31,7 @@ class Post(db.Model):
     def __repr__(self) -> str:
         return f"<Post {self.title}"
 
+
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
@@ -56,12 +46,12 @@ class Question(db.Model):
 
     def __repr__(self) -> str:
         return f"<Question {self.title}"
-    
+
 
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
-    question_id = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
+    question_id = db.Column(db.Integer, db.ForeignKey("question.id"), nullable=False)
     author = db.Column(db.String(80), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
     doctor_id = db.Column(db.Integer, db.ForeignKey('doctor.id'), nullable=True)
